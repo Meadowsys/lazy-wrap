b32362177f41bc6ccec8e5f0b9f4a6e319ebeb7b
Meadowsys <blazeykirin@gmail.com>
2024-02-17 18:32:33 -0800

    take inspiration off of std
    aha! i can use FnOnce like i originally tried!

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Smol wrapper around a type with an attached initialisation function. On first access, it'll call the attached function to initialise the value. Implements `Deref` and `DerefMut`.
 
-You can create this in a `static` variable. I've not really found any easier solution than to take a `&'static dyn Fn() -> T` as default type for `F` for the initialiser, so you would need to borrow on your closure. This would look something like:
+You can create this in a `static` variable. This would look something like:
 
 ```rs
 static STATIC_STRING: LazyWrap<String> = LazyWrap::new(&|| {
@@ -11,4 +11,4 @@ static STATIC_STRING: LazyWrap<String> = LazyWrap::new(&|| {
 });
 ```
 
-Please file issues if you find any, they would be very much appreciated c:
+I've done my best, but its not tested for soundness, so use at your own risk. Please do file issues if you find any however, it would be very much appreciated c:
--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,119 @@
-use ::parking_lot::Once;
+use ::parking_lot::{ Once, OnceState };
 use ::std::cell::UnsafeCell;
-use ::std::mem::MaybeUninit;
+use ::std::mem::ManuallyDrop;
 use ::std::ops::{ Deref, DerefMut };
 use ::std::panic::{ RefUnwindSafe, UnwindSafe };
+use ::std::ptr;
 
-pub struct LazyWrap<T, F = &'static dyn Fn() -> T> {
-	value: UnsafeCell<MaybeUninit<T>>,
-	init: F,
+union Data<T, F> {
+	init: ManuallyDrop<F>,
+	value: ManuallyDrop<T>
+}
+
+pub struct LazyWrap<T, F = fn() -> T> {
+	data: UnsafeCell<Data<T, F>>,
 	once: Once
+}
+
+pub enum LazyWrapState<T, F> {
+	Initialised(T),
+	Uninitialised(F)
 }
 
 impl<T, F> LazyWrap<T, F>
 where
-	F: Fn() -> T
+	F: FnOnce() -> T
 {
 	#[inline]
 	pub const fn new(init: F) -> Self {
-		let value = UnsafeCell::new(MaybeUninit::uninit());
+		let init = ManuallyDrop::new(init);
+		let data = UnsafeCell::new(Data { init });
 		let once = Once::new();
-		Self { value, init, once }
+		Self { data, once }
 	}
 
-	pub fn ensure_initialised(&self) {
-		self.once.call_once(|| {
-			let value = (self.init)();
-			unsafe { (*self.value.get()).write(value) };
+	#[inline]
+	pub fn ensure_initialised(this: &Self) {
+		this.once.call_once(|| {
+			let data = unsafe { &mut (*this.data.get()) };
+			let init = unsafe { ManuallyDrop::take(&mut data.init) };
+			let value = init();
+			data.value = ManuallyDrop::new(value);
 		});
 	}
 
 	#[inline]
-	pub fn is_initialised(&self) -> bool {
-		self.once.state().done()
+	fn ref_inner(this: &Self) -> &T {
+		Self::ensure_initialised(this);
+		unsafe { &(*this.data.get()).value }
+	}
+
+	#[inline]
+	fn mut_inner(this: &mut Self) -> &mut T {
+		Self::ensure_initialised(this);
+		unsafe { &mut (*this.data.get()).value }
+	}
+
+	#[inline]
+	pub fn is_initialised(this: &Self) -> bool {
+		use OnceState::*;
+		match this.once.state() {
+			New => { false }
+			Poisoned => { panic!("initialiser panicked") }
+			InProgress => {
+				this.once.call_once(|| {});
+				true
+			}
+			Done => { true }
+		}
+	}
+
+	pub fn into_inner(this: Self) -> LazyWrapState<T, F> {
+		let initialised = Self::is_initialised(&this);
+		let this = ManuallyDrop::new(this);
+		let data = unsafe { ptr::read(this.data.get()) };
+
+		if initialised {
+			LazyWrapState::Initialised(ManuallyDrop::into_inner(unsafe { data.value }))
+		} else {
+			LazyWrapState::Uninitialised(ManuallyDrop::into_inner(unsafe { data.init }))
+		}
+	}
+
+	pub fn into_inner_initialised(this: Self) -> T {
+		Self::ensure_initialised(&this);
+		let this = ManuallyDrop::new(this);
+		let data = unsafe { ptr::read(this.data.get()) };
+		ManuallyDrop::into_inner(unsafe { data.value })
 	}
 }
 
 impl<T, F> Deref for LazyWrap<T, F>
 where
-	F: Fn() -> T
+	F: FnOnce() -> T
 {
 	type Target = T;
 	#[inline]
 	fn deref(&self) -> &Self::Target {
-		self.ensure_initialised();
-		unsafe { (*self.value.get()).assume_init_ref() }
+		// ensure_initialised is called by ref_inner
+		Self::ref_inner(self)
 	}
 }
 
 impl<T, F> DerefMut for LazyWrap<T, F>
 where
-	F: Fn() -> T
+	F: FnOnce() -> T
 {
 	#[inline]
 	fn deref_mut(&mut self) -> &mut Self::Target {
-		self.ensure_initialised();
-		unsafe { (*self.value.get()).assume_init_mut() }
+		// ensure_initialised is called by mut_inner
+		Self::mut_inner(self)
 	}
 }
 
 impl<T, U, F> AsRef<U> for LazyWrap<T, F>
 where
-	F: Fn() -> T,
+	F: FnOnce() -> T,
 	T: AsRef<U>,
 	U: ?Sized
 {
@@ -72,7 +126,7 @@ where
 
 impl<T, U, F> AsMut<U> for LazyWrap<T, F>
 where
-	F: Fn() -> T,
+	F: FnOnce() -> T,
 	T: AsMut<U>,
 	U: ?Sized
 {
@@ -90,10 +144,24 @@ impl<T, F> RefUnwindSafe for LazyWrap<T, F> where T: RefUnwindSafe {}
 impl<T, F> Unpin for LazyWrap<T, F> where T: Unpin {}
 
 impl<T, F> Drop for LazyWrap<T, F> {
-	#[inline]
 	fn drop(&mut self) {
-		if self.once.state().done() {
-			unsafe { self.value.get_mut().assume_init_drop() }
+		use OnceState::*;
+		match self.once.state() {
+			New => {
+				unsafe { ManuallyDrop::drop(&mut self.data.get_mut().init) }
+			}
+			Poisoned => {}
+			InProgress => {
+				// ???
+				// lets drop the thing once its done just in case
+				// this cannot happen though. if we're dropping, we're the last one with a reference.
+
+				self.once.call_once(|| {});
+				unsafe { ManuallyDrop::drop(&mut self.data.get_mut().value) }
+			}
+			Done => {
+				unsafe { ManuallyDrop::drop(&mut self.data.get_mut().value) }
+			}
 		}
 	}
 }
